296128fd06d6abda095ab116a46e16e6edd70d3c
Brad Cypert <bradcypert@dawns-mac-mini.mediaura.net>
2012-02-20 13:48:33 -0500

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Brad Cypert on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Brad Cypert on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
